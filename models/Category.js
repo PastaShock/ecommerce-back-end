57735608f44976bcb82a15752fc731b01964ffff
George Pastushok <gpastushok@yahoo.com>
2022-05-20 10:36:29 -0700

    added category columns in models/categories

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,29 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
-  {
     // define columns
+  {
+    // column id:
+    id: {
+      // declare column data type as integer
+      type: DataTypes.INTEGER,
+      // set Null status of column
+      allowNull: false,
+      // set primary key status
+      primaryKey: true,
+      // set auto-increment
+      autoIncrement: true,
+    },
+// column category
+    cat_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
+  // set sequelize settings
   {
     sequelize,
     timestamps: false,
