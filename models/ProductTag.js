6543e90b327551d285a4f24a371260e527afe657
George Pastushok <gpastushok@yahoo.com>
2022-05-20 10:59:11 -0700

    added sequelize db routing/joining to models/index and completed tag, prodTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,44 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    // column key: id
+    id: {
+      // set datatype
+      type: DataTypes.INTEGER,
+      // allow null
+      allowNull: false,
+      // set primary status
+      primaryKey: true,
+      // set auto increment status
+      autoIncrement: true,
+    },
+    prod_id: {
+      // set datatype
+      type: DataTypes.INTEGER,
+      // allow null
+      allowNull: true,
+      // set foreign key reference:
+      references: {
+        // declare model name
+        model: 'product',
+        // call model primary key
+        key: 'id',
+      },
+    },
+    // column key: tag id
+    tag_id: {
+      // set datatype
+      type: DataTypes.INTEGER,
+      // allow null
+      allowNull: true,
+      // set foreign key reference:
+      references: {
+        // declare model name
+        model: 'tag',
+        // call model primary key
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,24 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    // column key: id
+    id: {
+      // set datatype
+      type: DataTypes.INTEGER,
+      // allow null
+      allowNull: false,
+      // set primary status
+      primaryKey: true,
+      // set auto increment status
+      autoIncrement: true,
+    },
+    // column key: tag name
+    tag_name: {
+      // set datatype
+      type: DataTypes.STRING,
+      // allow null
+      allowNull: false,
+    },
   },
   {
     sequelize,
