896afc862b4f980352078ad57ddc0c0ff5dd56ad
George Pastushok <gpastushok@yahoo.com>
2022-05-20 15:23:05 -0700

    completed product routes api

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,57 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  // call Product from  models and use the sequelize method findAll to get all products
+  Product.findAll({
+    // pull columns for each product
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+      // pull category for each product
+      model: Category,
+      attributes: ['category_name'],
+    },
+    {
+      // pull tag info for each product
+      model: Tag,
+      attributes: ['tag_name']
+    }]
+  }).then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    // get the id
+    where: {
+      id: req.params.id,
+    },
+    // pull columns for each product
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+      // pull category for each product
+      model: Category,
+      attributes: ['category_name'],
+    },
+    {
+      // pull tag info for each product
+      model: Tag,
+      attributes: ['tag_name']
+    }]
+  }).then(data => {
+    if(!data) {
+      res.status(404).json({message: 'product does not exist'});
+      return;
+    }
+    req.json(data);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -25,7 +69,14 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    // following the above product template we create a shape for the req/res
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds,
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -91,6 +142,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+    if(!data) {
+      res.status(404).json({message: 'product does not exist'});
+      return;
+    }
+    req.json(data);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
